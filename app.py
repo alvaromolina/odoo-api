8bb5e25aa8f51c8348f6498c4f2ed24c65a5dd72
Alvaro Molina <alvaromolina@Alvaros-MacBook-Pro.local>
2022-07-27 17:28:44 -0400

    create

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, json
 import xmlrpc.client
 
 app = Flask(__name__)
@@ -11,12 +11,22 @@ password = 'G$hT74d$C2dmWf'
 def hello_world():
     return 'Hello oddo-appi'
 
-@app.route('/get_customers')
+@app.route('/test')
 def get_customers():
     common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url))
-    print(common.version())
     uid = common.authenticate(db, username, password, {})
-    print(uid)
     models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url))
-    result = models.execute_kw(db, uid, password, 'res.partner', 'check_access_rights', ['read'], {'raise_exception': False})
+    #result = models.execute_kw(db, uid, password, 'res.partner', 'check_access_rights', ['read'], {'raise_exception': False})
+    result = models.execute_kw(db, uid, password, 'res.partner', 'fields_get', [], {'attributes': ['string', 'help', 'type']})
     return str(result)
+
+
+@app.route('/create', methods=['POST'])
+def create():
+    data = json.loads(request.data)
+    print(data)
+    common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url))
+    uid = common.authenticate(db, username, password, {})
+    models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url))
+    return_id = models.execute_kw(db,uid, password, 'res.partner', 'create', [data['data']])
+    return {'id' : return_id}
